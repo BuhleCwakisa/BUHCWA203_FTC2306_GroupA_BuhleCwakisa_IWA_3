13b6f6d55280763c1c0a36e146287f697be275b3
buhle <buhlec12@gmail.com>
2023-10-04 13:48:44 +0200

    challenge2

---
+++ script2.js
@@ -0,0 +1,7 @@
+// script2.js
+
+import {role as nwabisaRole} from "./Nwabisa.js"
+import {role as johanesRole} from "./Johannes.js"
+import {role as alexRole} from "./Alex.js" 
+
+console.log('Roles:', nwabisaRole, johanesRole, alexRole)
